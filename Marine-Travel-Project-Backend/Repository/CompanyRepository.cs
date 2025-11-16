8e50132dca53f419c93cb83adcc54cec6ce92ad3
Mikael Vestergaard Krogh <mikkro89@outlook.dk>
2025-11-16 11:01:01 +0100

    companyController implemented

--- Marine-Travel-Project-Backend/Repository/CompanyRepository.cs
+++ Marine-Travel-Project-Backend/Repository/CompanyRepository.cs
@@ -5,7 +5,7 @@ namespace Marine_Travel_Project_Backend.Repository;
 
 internal class CompanyRepository 
 {
-    public CompanyEntity? GetCompany(Guid companyId)
+    public CompanyEntity? Get(Guid companyId)
     {
         if (companyId == Guid.Empty)
         {
